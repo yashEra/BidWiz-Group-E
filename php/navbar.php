ee1f830b3ccfa05e4eeb7e30c96652acf54ca89f
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2023-12-01 18:18:31 +0530

    Update

--- php/navbar.php
+++ php/navbar.php
@@ -22,7 +22,7 @@ session_start();
 <body>
 
 	<navbar class="navbar__section">
-	<header class="nav__header">
+		<header class="nav__header">
 			<nav class="navN nav__container">
 				<div class="nav__data">
 					<a href="../index.php" class="nav__logo">
@@ -37,7 +37,7 @@ session_start();
 				<div class="nav__menu" id="nav-menu">
 					<ul class="nav__list">
 						<li class="dropdown__item">
-						<a href="category.php" class="nav__link" style="text-decoration: none;">Categories</a>
+							<a href="category.php" class="nav__link" style="text-decoration: none;">Categories</a>
 
 						</li>
 
@@ -66,15 +66,12 @@ session_start();
 
 						<li class="dropdown__item">
 							<?php
-							require_once './classes/person.php';
-							// session_start();
-
 							if (isset($_SESSION["buyer"])) {
 								$buyer = $_SESSION["buyer"];
-								echo '<a href="buyer_profile.php" class="nav__link">' . $buyer->getFirstName() . '<i style="padding-left: 20px;" class="fa fa-user" aria-hidden="true"></i></a>';
+								echo '<a href="buyer_profile.php" class="nav__link">' . $buyer->getFirstName() . '<img src="' . $buyer->getpic() . '" alt="Buyer" class="rounded-circle p-1 bg-primary" width="50"></a>';
 							} elseif (isset($_SESSION["seller"])) {
 								$seller = $_SESSION["seller"];
-								echo '<a href="seller_profile.php" class="nav__link">' . $seller->getFirstName() . '<i style="padding-left: 20px;" class="fa fa-user" aria-hidden="true"></i></a>';
+								echo '<a href="seller_profile.php" class="nav__link">' . $seller->getFirstName() . '<img src="' . $seller->getpic() . '" alt="Seller" class="rounded-circle p-1 bg-primary m-2" width="50"></a>';
 							} else {
 								echo '<a href="prelogin.php" class="nav__link">Login/Signup<i style="padding-left: 20px;" class="fa fa-user" aria-hidden="true"></i></a>';
 							}
